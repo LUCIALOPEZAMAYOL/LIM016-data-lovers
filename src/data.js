309d344b7251cc865d414930d3abd2c5a4be61fa
unknown <llopeza@alumno.unsm.edu.pe>
2021-11-20 03:03:54 -0500

    prueba de ordenar pokemones

--- src/data.js
+++ src/data.js
@@ -12,7 +12,35 @@
 
 //Funcionalidad de ordenar pokemon
 
-export const ordenarPor = () => {
-  return 'OMG';
-};
+/*export const ordenarPokemones = (dataPokemon,nombre) => {
+    let pokemonesOrdenados=[];
+    if(nombre==='name'){
+      if(ordenar==='a-z'){
+        pokemonesOrdenados=data.pokemon.sort(function(a,b){
+          if(a.name > b.name) return 1;
+          if(a.name === b.name) return 0;
+          return -1;
+        });
+      } else {
+        pokemonesOrdenados=data.pokemon(function(a,b){
+          if(a.name>b.name) return 1;
+          if(a.name===b.name)return 0;
+          return -1;
+        })
+      }
+      }};*/
+
+      export const filtradoTipo = (datapokemones, nombre) => {
+
+      const pokemonesOrdenados= datapokemones.sort((a,b)=>{
+        if(a.nombre < b.nombre){
+            return -1;
+        }
+        if(a.nombre >b.nombre){
+            return 1;
+        }
+        return 0;
+
+      }
+      )};
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
-import filtradoTipo from './data.js';
-import ordenarPor from './data.js';
+import {filtradoTipo, ordenarPokemones} from './data.js';
 /*import buscarPokemon from './data.js';*/
 import data from './data/pokemon/pokemon.js';
 
@@ -35,6 +34,38 @@ const submenus=document.querySelectorAll('.submenu');
             let pokemonesFiltrados= filtradoTipo(data.pokemon,tipo);
             mostrarCards(pokemonesFiltrados);
         })
-    }
+    };
+    const todos= document.getElementById('todos');
+    todos.addEventListener('click', ()=>{
+        mostrarCards(data.pokemon);
+    });
 
+//Funcionalidad para ordenar los pokemon
+
+/*const sortData=document.querySelectorAll('.ordenar');
+for(let i=0; i<sortData.length; i++){
+    sortData[i].addEventListener('click', ()=>{
+        let nombre=sortData[i];
+        let pokemonesOrdenados = ordenarPokemones(data.pokemon, nombre);
+        mostrarCards(pokemonesOrdenados);
+    })
+};*/
+/*document.getElementById('a-z').addEventListener('click', ()=>{
+    
+    })
+    mostrarCards(pokemonesOrdenados);
+})*/
+
+document.getElementById('z-a').addEventListener('click', ()=>{
+    const pokemonesOrdenados= data.pokemon.sort((a,b)=>{
+        if(a.name < b.name){
+            return 1;
+        }
+        if(a.name >b.name){
+            return -1;
+        }
+        return 0;
+    })
+    mostrarCards(pokemonesOrdenados);
+})
 
